a419a66eb8d381acb82531391b2a83ae0fae9fbe
Yoko Harada <yokolet@gmail.com>
2017-11-12 13:16:14 +0900

    feat: adds templates for five components
    Five components were added, but those are just a template and don't do
    anything meaningful at this moment.

---
+++ components/DeckList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function DeckList () {
+  return (
+    <View>
+      <Text>DeckList View</Text>
+    </View>
+  )
+}
