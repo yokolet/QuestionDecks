694b1e4aa9db0453318aa281a87bc3702107e415
Yoko Harada <yokolet@gmail.com>
2017-11-12 13:54:25 +0900

    feat: adds status bar
    The status bar has been added. The bar may have another color or some
    parameters later. For now, the very simple one is there.

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar, NativeModules } from 'react-native'
+import { white } from './utils/colors'
+import { TabNavigator, StackNavigator } from 'react-navigation'
+
+const { StatusBarManager } = NativeModules
+function MobileFlashcardsStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: StatusBarManager.HEIGHT }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <MobileFlashcardsStatusBar backgroundColor={white} barStyle='dark-content' hidden={false}/>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
       </View>
     );
   }
