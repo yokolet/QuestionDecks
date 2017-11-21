90c4de259ea1b8c2bb589815dec212fc676376e0
YOKO HARADA <yokolet@gmail.com>
2017-11-21 15:06:28 +0900

    feat: adds style and buttons to deck view
    An individual deck view had a style and two buttons. However, when
    zero card is there, start quiz button won't show up.

--- components/Deck.js
+++ components/Deck.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { connect } from 'react-redux'
+import { purple, white, gray1, gray3, gray5, black } from '../utils/colors'
 
 class Deck extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -9,13 +10,25 @@ class Deck extends Component {
 
   render () {
     const { entries, deckId } = this.props
+    let canStartQuize = entries[deckId].cards.length > 0 ? true : false
     return (
       <View style={styles.container}>
-        <Text>Deck View</Text>
         {entries && deckId &&
-          <View>
-            <Text>{entries[deckId].name}</Text>
-            <Text>{entries[deckId].cards.length} cards</Text>
+          <View style={styles.deck}>
+            <Text style={styles.deckTitle}>{entries[deckId].name}</Text>
+            <Text style={styles.deckInfo}>{entries[deckId].cards.length} cards</Text>
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={[styles.button, {backgroundColor: white}]}>
+              <Text style={[styles.buttonText, {color: black}]}>Add Card</Text>
+            </TouchableOpacity>
+            { canStartQuize &&
+              <TouchableOpacity
+                style={[styles.button, {backgroundColor: black}]}>
+                <Text style={[styles.buttonText, {color: white}]}>Start Quiz</Text>
+              </TouchableOpacity>
+            }
+            </View>
           </View>
         }
       </View>
@@ -26,9 +39,57 @@ class Deck extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  deck: {
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: white,
+    borderColor: gray5,
+    borderWidth: 1,
+    padding: 30,
+    margin: 5,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgba(0, 0, 0, .2)',
+        shadowOffset:  { height: 0, width: 0 },
+        shadowOpacity: 1,
+        shadowRadius: 1,
+      },
+      android: {
+        elevation: 1,
+      },
+    })
   },
+  deckTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 15,
+    color: gray1,
+  },
+  deckInfo: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: gray3,
+  },
+  buttonContainer: {
+    margin: 50,
+    marginTop: 300,
+  },
+  button: {
+    width: 200,
+    margin:10,
+    padding: 20,
+    paddingLeft: 50,
+    paddingRight: 50,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: black,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 15,
+  }
 });
 
 function mapStateToProps (deckData) {
--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 
-export default function NewDeck () {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>New Deck View</Text>
-    </View>
-  )
+class NewDeck extends Component {
+  render () {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>New Deck View</Text>
+      </View>
+    )
+  }
 }
+
+export default NewDeck
